ab7fd762209c33f68d13586d1a72191ea04e37ed
Junmo Kang <rapier83@gmail.com>
2017-10-31 00:12:32 +0900

    Initial Commit

---
+++ NodeWork/Document.swift
@@ -0,0 +1,29 @@
+//
+//  Document.swift
+//  NodeWork
+//
+//  Created by Junmo Kang on 31/10/2017.
+//  Copyright © 2017 Junmo Kang. All rights reserved.
+//
+
+import Cocoa
+
+class Document: NSPersistentDocument {
+
+    override init() {
+        super.init()
+        // Add your subclass-specific initialization here.
+    }
+
+    override class var autosavesInPlace: Bool {
+        return true
+    }
+
+    override func makeWindowControllers() {
+        // Returns the Storyboard that contains your Document window.
+        let storyboard = NSStoryboard(name: NSStoryboard.Name("Main"), bundle: nil)
+        let windowController = storyboard.instantiateController(withIdentifier: NSStoryboard.SceneIdentifier("Document Window Controller")) as! NSWindowController
+        self.addWindowController(windowController)
+    }
+
+}
---
+++ NodeWork/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NodeWork
+//
+//  Created by Junmo Kang on 31/10/2017.
+//  Copyright © 2017 Junmo Kang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
